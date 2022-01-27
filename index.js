746b5c680b64c5201db04dcf148e3e92539c6b2d
0xherman <0xhermandev@gmail.com>
2022-01-27 13:18:22 -0800

    init

---
+++ index.js
@@ -0,0 +1,185 @@
+const abi = [
+	{
+		"inputs": [],
+		"name": "claimDividend",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "shareholder",
+				"type": "address"
+			}
+		],
+		"name": "getUnpaidEarnings",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const WalletConnector = function (messageBox, connectWalletButton, disconnectWalletButton) {
+	this.dividendAddress = new URLSearchParams(window.location.search).get("addr"),
+	this.messageBox = messageBox ? messageBox : document.getElementById('message'),
+	this.connectWalletButton = connectWalletButton ? connectWalletButton : document.getElementById('connect-wallet'),
+	this.disconnectWalletButton = disconnectWalletButton ? disconnectWalletButton : document.getElementById('disconnect-wallet'),
+	this.claimDividendsButton = document.getElementById("claim-dividends"),
+	this.refreshButton = document.getElementById("refresh-data"),
+	this.account = "",
+	this.walletConnected = false,
+	this.init = function () {
+		if (!(this.connectWalletButton && this.connectWalletButton instanceof Element)) {
+			throw "connectWalletButton must be a DOM Element"
+		}
+		if (!(this.disconnectWalletButton && this.disconnectWalletButton instanceof Element)) {
+			throw "disconnectWalletButton must be a DOM Element"
+		}
+		this.connectWalletButton.addEventListener('click', () => {
+			this.connectWallet()
+		})
+		this.disconnectWalletButton.addEventListener('click', () => {
+			this.disconnectWallet()
+		})
+		this.claimDividendsButton.addEventListener('click', () => { 
+			this.claimDividends()
+		})
+		this.refreshButton.addEventListener('click', () => {
+			this.loadData()
+		})
+	},
+	this.checkProviderIfLoggedIn = async function () {
+		if (this.walletConnected && providerLoggedInWith == 'walletconnect') {
+			Web3Provider = await web3Modal.connectTo('walletconnect')
+		}
+	},
+	this.showMessage = function (message) {
+		this.messageBox.innerText = message
+	},
+	this.connectWallet = async function () {
+		try {
+
+			Web3Provider = await web3Modal.connect()
+			Web3Instance = new Web3(Web3Provider)
+			await this.loadData()
+		} catch (e) {
+			if (e.code == "4001") {
+				this.showMessage(e.message)
+			} else {
+				this.showMessage("Couldn't get the wallet connection")
+			}
+			console.log("Couldn't get a wallet connection")
+			console.log(e)
+		}
+
+		this.initProviderEvents();
+	},
+	this.loadData = async function() {
+		this.walletConnected = true
+		this.account = await this.fetchAccountData()
+		let message = "Wallet: " + this.account
+		if (this.dividendAddress) {
+			console.log(this.dividendAddress)
+			const pending = await this.fetchPendingDividends(this.account)
+			document.getElementById("claim").style.display = "block";
+			message += "\nPending Rewards: " + pending
+		}
+		this.showMessage(message)
+	},
+	this.disconnectWallet = async function () {
+
+		
+
+		// Try to close the web3 session 
+		try {
+			await Web3Provider.close()
+		} catch (e) {
+			this.showMessage("Disconnected");
+			document.getElementById("claim").style.display = "none";
+		}
+
+		// Also clear the cacheProvider so it asks for wallet options again, instead of asking the login of previous provider
+		try {
+			await web3Modal.clearCachedProvider();
+		} catch (e) {
+			console.log(e)
+		}
+
+	},
+	this.claimDividends = async function () {
+		try {
+			console.log(this.account, this.dividendAddress, abi)
+			const contract = new Web3Instance.eth.Contract(abi, this.dividendAddress);
+			console.log(this.account)
+			await contract.methods.claimDividend().send({ from: this.account })
+			await this.loadData()
+		}
+		catch (e) {
+			console.log(e)
+			alert('Failed to claim!')
+		}
+	},
+	this.fetchAccountData = async function () {
+		// Get list of accounts of the connected wallet
+		const accounts = await Web3Instance.eth.getAccounts()
+
+		selectedAccount = accounts[0].toLowerCase()
+		return selectedAccount
+	},
+	this.fetchPendingDividends = async function (account) {
+		console.log(account, this.dividendAddress, abi)
+		const contract = new Web3Instance.eth.Contract(abi, this.dividendAddress, { from: account });
+		const pending = await contract.methods.getUnpaidEarnings(account).call({ from: account });
+		console.log(pending)
+		console.log("init")
+		return Web3Instance.utils.fromWei(pending);
+	}
+
+	this.initProviderEvents = function () {
+
+		// Subscribe to accounts change
+		Web3Provider.on("networkChanged", (accounts) => {
+			console.log("networkChanged");
+			console.log(accounts);
+		});
+
+		Web3Provider.on("accountsChanged", (accounts) => {
+			console.log("accountsChanged");
+			console.log(accounts);
+		});
+
+		// Subscribe to chainId change
+		Web3Provider.on("chainChanged", (chainId) => {
+			console.log("chainChanged");
+
+			console.log(chainId);
+		});
+
+		// Subscribe to provider connection
+		Web3Provider.on("connect", (info) => {
+			console.log("connect");
+
+			console.log(info);
+		});
+
+		// Subscribe to provider disconnection
+		Web3Provider.on("disconnect", (error) => {
+			this.showMessage("Disconnected")
+			document.getElementById("claim").style.display = "block";
+			console.log("disconnect");
+			console.log(error);
+		});
+
+	}
+}
+
+let walletConnector = new WalletConnector()
+walletConnector.init();
---
+++ provider.js
@@ -0,0 +1,48 @@
+//  You have to refer to default since it was bundled for ESModules
+// but after that the documentation will be the same
+const Web3Modal = window.Web3Modal.default;
+const WalletConnectProvider = window.WalletConnectProvider.default;
+
+let Web3Provider;
+let Web3Instance;
+
+const providerOptions = {
+	walletconnect: {
+		package: WalletConnectProvider,
+		options: {
+			rpc: {
+				43114: 'https://speedy-nodes-nyc.moralis.io/ec9afe6028435732c70a61dc/avalanche/mainnet'
+			},
+			network: 'avalanche-fuji-mainnet',
+		}
+	},
+	'custom-walletlink': {
+		display: {
+			logo: 'https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0',
+			name: 'Coinbase',
+			description: 'Connect to Coinbase Wallet (not Coinbase App)',
+		},
+		options: {
+			appName: 'Coinbase', // Your app name
+			networkUrl: `https://speedy-nodes-nyc.moralis.io/ec9afe6028435732c70a61dc/avalanche/mainnet`,
+			chainId: 43114,
+		},
+		package: WalletLink,
+		connector: async (_, options) => {
+			const { appName, networkUrl, chainId } = options
+			const walletLink = new WalletLink({
+				appName,
+			})
+			const provider = walletLink.makeWeb3Provider(networkUrl, chainId)
+			await provider.enable()
+			return provider
+		},
+	},
+	/* See Provider Options Section */
+};
+
+const web3Modal = new Web3Modal({
+	network: 43114,
+	cacheProvider: false, // optional
+	providerOptions // required
+});
